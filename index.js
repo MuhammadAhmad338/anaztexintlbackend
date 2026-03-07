f5ea6d92885161f53385edb041c2d01d47588a92
MuhammadAhmad338 <ahmadmuhammad.7700@gmail.com>
2026-03-07 16:49:54 +0500

    increased the json size

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true // if you send cookies/auth headers
 }));
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use('/uploads', express.static('uploads')); // Serve uploaded files
 
 // Database Connection
