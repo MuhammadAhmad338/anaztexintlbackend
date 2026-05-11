35fbbb75ce8350076cd9dce6fe8502816c1501f7
MuhammadAhmad338 <ahmadmuhammad.7700@gmail.com>
2026-05-11 15:55:19 +0500

    latestcommits

--- Middleware/trackingidcreation.js
+++ Middleware/trackingidcreation.js
@@ -1,4 +1,5 @@
 const { nanoid } = require("nanoid");
+
 function generateTrackingNumber(country = "UK") {
   return `${country}-${Date.now().toString(36).toUpperCase()}-${nanoid(6).toUpperCase()}`;
 }
